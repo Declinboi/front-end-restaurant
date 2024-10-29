598c39aba2bda70fe139663d9648ca23e42951c4
declinboi <jacobclinton2234@gmail.com>
2024-10-29 06:18:05 +0100

    yea

--- front-end/src/pages/About.jsx
+++ front-end/src/pages/About.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+import AboutUs from '../components/AboutUs'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <Navbar/>
+      <AboutUs/>
+    </div>
   )
 }
 
